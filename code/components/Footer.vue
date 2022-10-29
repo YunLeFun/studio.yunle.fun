785a98428bb8f39b41e4b9bb21366c5ce35f0fe9
YunYouJun <me@yunyoujun.cn>
2022-10-30 05:11:40 +0800

    feat: add bilibili and wechat link

--- code/components/Footer.vue
+++ code/components/Footer.vue
@@ -10,7 +10,9 @@ const icp = '京ICP备2022027075号-1'
     justify-center
     gap-3
   >
-    <a i-carbon-logo-github href="https://github.com/YunLeFun" target="_blank" />
+    <a i-ri-github-line href="https://github.com/YunLeFun" title="YunLeFun" target="_blank" />
+    <a i-ri-bilibili-line href="https://space.bilibili.com/165596026" title="云乐坊工作室" target="_blank" />
+    <a i-ri-wechat-line href="/images/yunlefun-wechat.jpg" title="云乐坊工作室" target="_blank" />
   </div>
   <div class="mt-5 mx-auto text-center opacity-60 text-sm">
     <a href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank">
